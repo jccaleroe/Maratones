bead59ae6e408d2d499787233f5d1b772063636d
jccaleroe <jccaleroe@unal.edu.co>
2017-07-18 17:39:23 -0500

    relations

--- Rogue/src/Wooombat/Syntaxnet/Syntaxnet.java
+++ Rogue/src/Wooombat/Syntaxnet/Syntaxnet.java
@@ -6,8 +6,8 @@ import java.io.InputStreamReader;
 
 public class Syntaxnet {
 
-    private static StringBuilder txt = new StringBuilder(), ann = new StringBuilder();
-    private static long txtIndex = 0, boundIndex = 1;
+    private static StringBuilder txt = new StringBuilder(), ann = new StringBuilder(), relations = new StringBuilder();
+    private static long txtIndex = 0, boundIndex = 1, relationsIndex = 1;
 
     static int start(){
         String s;
@@ -31,6 +31,7 @@ public class Syntaxnet {
         String[] tmp = str.split("\\s+");
         txt.append(tmp[1]);
         txt.append(" ");
+
         ann.append("T");
         ann.append(boundIndex++);
         ann.append("\t");
@@ -43,6 +44,19 @@ public class Syntaxnet {
         ann.append("\t");
         ann.append(tmp[1]);
         ann.append("\n");
+
+        String aux = tmp[6];
+        if (aux.equals("0")) return;
+
+        relations.append("R");
+        relations.append(relationsIndex++);
+        relations.append("\t");
+        relations.append(tmp[7]);
+        relations.append(" Arg1:T");
+        relations.append(aux);
+        relations.append(" Arg2:T");
+        relations.append(boundIndex - 1);
+        relations.append("\n");
     }
 
     private static int writeFiles(String fileName){
@@ -57,7 +71,7 @@ public class Syntaxnet {
             br.close();
             process.destroy();
             process = Runtime.getRuntime().exec(new String[]{"docker", "exec", "brat", "sh",
-                    "bratdata/copy.sh", ann.toString(), "bratdata/"+fileName+".ann"});
+                    "bratdata/copy.sh", ann.toString() + relations.toString(), "bratdata/"+fileName+".ann"});
             br = new BufferedReader(new InputStreamReader(process.getInputStream()));
             while ((str = br.readLine()) != null) System.out.println(str);
             process.waitFor();
@@ -78,9 +92,7 @@ public class Syntaxnet {
             Process process = Runtime.getRuntime().exec(new String[]{"docker", "exec", "syntaxnet", "sh",
                     "syntaxnet/spanish.sh", text});
             BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            while ((str = br.readLine()) != null && !str.isEmpty()) {
-                System.out.println(str);
-                convert(str);}
+            while ((str = br.readLine()) != null && !str.isEmpty()) convert(str);
             process.waitFor();
             exit = process.exitValue();
             br.close();
@@ -95,7 +107,7 @@ public class Syntaxnet {
 
     public static void main(String[] args) {
         //System.out.println("Process finished with exit code " + start());
-        int exit = execute("Ivan es el mejor bailador", "foo");
+        int exit = execute("yo declaro que Ivan es el mejor bailarin", "foo3");
         System.out.println("Process finished with exit code " + exit);
     }
 }
